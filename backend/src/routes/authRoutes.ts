9944f1e1a3e1b2ae09f5c0d78e5e891caae8f956
Adekola Adediwura <adekolaadediwura@gmail.com>
2025-10-01 01:25:43 +0100

    added the route protector function  and authroutes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 const router = Router();
-import { login, register } from "../controllers/authController.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
